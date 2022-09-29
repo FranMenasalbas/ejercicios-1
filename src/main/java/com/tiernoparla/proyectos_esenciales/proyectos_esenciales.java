0a80288d5f570f9ec1ae4cd1f4cb43c1dcae578e
sara-delacasa-tierno <sara.delacasalanza@tiernoparla.es>
2022-09-29 18:06:56 +0200

    he modificado el src

--- src/main/java/com/tiernoparla/proyectos_esenciales/proyectos_esenciales.java
+++ src/main/java/com/tiernoparla/proyectos_esenciales/proyectos_esenciales.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tiernoparla.proyectos_esenciales;
 
-/**
- *
- * @author alumno
- */
+import java.util.Scanner;
+        
+        
 public class proyectos_esenciales {
     
-}
+    public static void main(String[] args) {
+
+        
+        //números pares del 1 al 10
+        /*
+        //números del 1 al 10 pero de otra forma
+        for(int n = 1; n <=10; n++){
+            if(n%2==0){
+                System.out.println(n);
+            }//if
+        }//for
+        */
+        
+        
+        //división pero le da el usuario los datos
+        /*
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("dame un número entero para que sea dividido: ");
+        n = sc.nextInt();
+        Scanner sc2 = new Scanner(System.in);
+        int d;
+        System.out.println("dame un número entero para dividir el anterior: ");
+        d = sc2.nextInt();
+        int r=n;
+        int q=0;
+        while (r>=d){
+            r = r-d;
+            q++;
+        }//while
+        System.out.println(q);
+        System.out.println(n == d*q+r);
+        */
+        
+        
+        //
+        
+        
+    }//main
+}//public class
