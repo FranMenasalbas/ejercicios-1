22a5a3fbeea561690504a088d15cd266181ae92a
sara-delacasa-tierno <sara.delacasalanza@tiernoparla.es>
2022-10-03 21:37:32 +0200

    modifico

--- src/main/java/com/tiernoparla/proyectos_esenciales/proyectos_esenciales.java
+++ src/main/java/com/tiernoparla/proyectos_esenciales/proyectos_esenciales.java
@@ -40,8 +40,40 @@ public class proyectos_esenciales {
         */
         
         
-        //
+        //Números de fibonach (machacar variables.
+        /*
+        int num = 55;//cuantos números quiero que saque el código
+        int x1 =1, x2 = 1;
+        int suma = 1;
+        for (int n = 1; n < num; n++) {
+            suma = x1 + x2;
+            x1 = x2;
+            x2 = suma;
+        }//for
+        System.out.println(suma);
+        */
         
+        //arrays: son colecciones de datos, una lista de tamao fijo vaya
+        /*string hace que la array sea solo para letras, 
+        para números susar por ejemplo int y recordar q int no usa comillas*/
+        /*
+        String[] p={"uno"/*este es el sitio cero*//*,"dos" /*este es el 
+        sitio uno y así};
+        System.out.println(p[0]);
+        System.out.println(p[1]);
+        */
+        //calcular termino n de fibonacci con arraysnen vez de x1, x2 y suma
+        //calcular y guardar todos los terminos de fibonacci hasta el n
+        /*
+        int num = 5;
+        int[] x={1,1,0};
+        for (int i = 1; i < num; i++) {
+            x[2] = x[0] + x[1];
+            x[0] = x[1];
+            x[1] = x[2];
+        }//for
+        System.out.println(x[2]);
+        */
         
     }//main
 }//public class
